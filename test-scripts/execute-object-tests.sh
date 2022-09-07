f73aefa612b34bb6efdc77447d608e821667a7e5
Christoph Hagelkruys <christoph.hagelkruys@alanda.io>
2022-09-08 00:15:19 +0200

    FINAL Bachelor Thesis commit

--- test-scripts/execute-object-tests.sh
+++ test-scripts/execute-object-tests.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-#type-tests
-/bin/bash ./execute-test-plan mongodb delete-type type-tests
-/bin/bash ./execute-test-plan postgres delete-type type-tests
+/bin/bash ./execute-test-plan.sh mongodb delete-object object-tests
+/bin/bash ./execute-test-plan.sh postgres delete-object object-tests
 
-/bin/bash ./execute-test-plan mongodb create-type type-tests
-/bin/bash ./execute-test-plan postgres create-type type-tests
+/bin/bash ./execute-test-plan.sh mongodb create-object object-tests
+/bin/bash ./execute-test-plan.sh postgres create-object object-tests
 
-/bin/bash ./execute-test-plan mongodb get-all-types type-tests
-/bin/bash ./execute-test-plan postgres get-all-types type-tests
+/bin/bash ./execute-test-plan.sh mongodb get-object object-tests
+/bin/bash ./execute-test-plan.sh postgres get-object object-tests
 
-/bin/bash ./execute-test-plan.sh mongodb get-type type-tests
-/bin/bash ./execute-test-plan.sh postgres get-type type-tests
-
-/bin/bash ./execute-test-plan mongodb update-type type-tests
-/bin/bash ./execute-test-plan postgres update-type type-tests
-
-#object tests
+/bin/bash ./execute-test-plan.sh mongodb update-object object-tests
+/bin/bash ./execute-test-plan.sh postgres update-object object-tests
--- test-scripts/execute-test-plan.sh
+++ test-scripts/execute-test-plan.sh
@@ -3,7 +3,7 @@
 test_db_name=$1 #mongodb or postgres
 test_name=$2 #delete-type
 test_object=$3 #type-tests or object-tests
-test_src_path=~/developers-happy-place/FH/ba/object-store-${test_db_name}-service
+test_src_path=../../object-store-${test_db_name}-service
 
 test_type="100x200"
 
@@ -13,11 +13,11 @@ cd ${test_src_path}
 ./startup.sh
 sleep 1m
 
-cd ~/developers-happy-place/FH/ba/end-tests/apache-jmeter-5.5-SNAPSHOT/bin
-mkdir -p ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}
-rm -rf ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}/*
+cd ../apache-jmeter-5.5-SNAPSHOT/bin
+mkdir -p ../${test_object}/${test_name}/${test_type}/${test_db_name}
+rm -rf ../${test_object}/${test_name}/${test_type}/${test_db_name}/*
 
-sh jmeter -n -t ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/jmeter-test-scenarios/${test_type}.jmx -l ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}/${test_db_name}.csv -e -o ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}
+sh jmeter -n -t ../${test_object}/${test_name}/jmeter-test-scenarios/${test_type}.jmx -l ../${test_object}/${test_name}/${test_type}/${test_db_name}/${test_db_name}.csv -e -o ../${test_object}/${test_name}/${test_type}/${test_db_name}
 
 cd ${test_src_path}
 ./shutdown.sh
@@ -29,11 +29,11 @@ cd ${test_src_path}
 ./startup.sh
 sleep 1m
 
-cd ~/developers-happy-place/FH/ba/end-tests/apache-jmeter-5.5-SNAPSHOT/bin
-mkdir -p ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}
-rm -rf ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}/*
+cd ../apache-jmeter-5.5-SNAPSHOT/bin
+mkdir -p ../${test_object}/${test_name}/${test_type}/${test_db_name}
+rm -rf ../${test_object}/${test_name}/${test_type}/${test_db_name}/*
 
-sh jmeter -n -t ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/jmeter-test-scenarios/${test_type}.jmx -l ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}/${test_db_name}.csv -e -o ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}
+sh jmeter -n -t ../${test_object}/${test_name}/jmeter-test-scenarios/${test_type}.jmx -l ../${test_object}/${test_name}/${test_type}/${test_db_name}/${test_db_name}.csv -e -o ../${test_object}/${test_name}/${test_type}/${test_db_name}
 
 cd ${test_src_path}
 ./shutdown.sh
@@ -45,11 +45,11 @@ cd ${test_src_path}
 ./startup.sh
 sleep 1m
 
-cd ~/developers-happy-place/FH/ba/end-tests/apache-jmeter-5.5-SNAPSHOT/bin
-mkdir -p ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}
-rm -rf ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}/*
+cd ../apache-jmeter-5.5-SNAPSHOT/bin
+mkdir -p ../${test_object}/${test_name}/${test_type}/${test_db_name}
+rm -rf ../${test_object}/${test_name}/${test_type}/${test_db_name}/*
 
-sh jmeter -n -t ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/jmeter-test-scenarios/${test_type}.jmx -l ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}/${test_db_name}.csv -e -o ~/developers-happy-place/FH/ba/end-tests/${test_object}/${test_name}/${test_type}/${test_db_name}
+sh jmeter -n -t ../${test_object}/${test_name}/jmeter-test-scenarios/${test_type}.jmx -l ../${test_object}/${test_name}/${test_type}/${test_db_name}/${test_db_name}.csv -e -o ../${test_object}/${test_name}/${test_type}/${test_db_name}
 
 cd ${test_src_path}
 ./shutdown.sh
